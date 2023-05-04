7756b4460f00a6b44e0b06924fe3c7ff8f4c4ed4
Gregorius Ongo <gregoriusongo@gmail.com>
2023-05-04 21:07:14 +0700

    add home pages

---
+++ src/components/Suggestion.js
@@ -0,0 +1,78 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Suggestion.css";
+
+function Suggestion() {
+   return (
+      <div className="suggestions">
+         <div className="profile">
+            <Avatar>
+               U
+            </Avatar>
+            <div>Username</div>
+            <div>User Full Name</div>
+            <button className="switch__button">Switch</button>
+         </div>
+         <div className="suggestions__title">Suggested for you
+            <span className="suggestions__title__all">
+               See All
+            </span>
+         </div>
+         <div className="suggestions__usernames">
+            <div className="suggestions__username">
+               <div className="username__left">
+                  <span className="avatar">
+                     <Avatar>SU</Avatar>
+                  </span>
+                  <div className="username__info">
+                     <span className="username">suggested_user1</span>
+                     <span className="relation">Followed by your_friend + 3 more</span>
+                  </div>
+               </div>
+               <button className="follow__button">Follow</button>
+            </div>
+
+            <div className="suggestions__username">
+               <div className="username__left">
+                  <span className="avatar">
+                     <Avatar>SU</Avatar>
+                  </span>
+                  <div className="username__info">
+                     <span className="username">suggested_user2</span>
+                     <span className="relation">New to Instagram</span>
+                  </div>
+               </div>
+               <button className="follow__button">Follow</button>
+            </div>
+
+            <div className="suggestions__username">
+               <div className="username__left">
+                  <span className="avatar">
+                     <Avatar>SU</Avatar>
+                  </span>
+                  <div className="username__info">
+                     <span className="username">suggested_user3</span>
+                     <span className="relation">New to Instagram</span>
+                  </div>
+               </div>
+               <button className="follow__button">Follow</button>
+            </div>
+
+            <div className="suggestions__username">
+               <div className="username__left">
+                  <span className="avatar">
+                     <Avatar>SU</Avatar>
+                  </span>
+                  <div className="username__info">
+                     <span className="username">suggested_user4</span>
+                     <span className="relation">New to Instagram</span>
+                  </div>
+               </div>
+               <button className="follow__button">Follow</button>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default Suggestion;
---
+++ src/components/Timeline.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import Post from "./Post";
+import "./Timeline.css";
+import Suggestion from "./Suggestion";
+
+function Timeline() {
+   const [posts, setPosts] = useState([
+      {
+         user: "user1",
+         postImage:
+            "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
+         likes: 5,
+         timestamp: "2d",
+         caption: "lorem ipsum",
+      },
+      {
+         user: "user2",
+         postImage:
+            "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
+         likes: 100,
+         timestamp: "2d",
+         caption: "lorem ipsum",
+      },
+      {
+         user: "user3",
+         postImage:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+         likes: 2000,
+         timestamp: "2d",
+         caption: "lorem ipsum",
+      },
+      {
+         user: "user4",
+         postImage:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGCAaQ5u1TMTij5ELPWi5-VPtlSqELw-R6lj0EpYmNcGt56kOQaCokzS0IK81MOSphlkw&usqp=CAU",
+         likes: 534,
+         timestamp: "2d",
+         caption: "lorem ipsum",
+      },
+   ]);
+
+   return (
+      <div className="timeline">
+         <div className="timeline__left">
+            <div className="timeline__posts">
+               {posts.map((post) => (
+                  <Post
+                     user={post.user}
+                     postImage={post.postImage}
+                     likes={post.likes}
+                     timestamp={post.timestamp}
+                  />
+               ))}
+            </div>
+         </div>
+         <div className="timeline__right">
+            <Suggestion />
+         </div>
+      </div>
+   );
+}
+
+export default Timeline;
